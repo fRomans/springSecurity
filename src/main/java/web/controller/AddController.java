8ab2a859777098ee75b52b3b43f351fa1ee68ca6
Роман <vodaz@mail.ru>
2020-07-14 12:25:42 +0300

    14/07_12:25

--- src/main/java/web/controller/AddController.java
+++ src/main/java/web/controller/AddController.java
@@ -1,7 +1,7 @@
 package web.controller;
 
-import com.javamaster.model.User;
-import com.javamaster.service.UserService;
+import web.model.User;
+import web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
